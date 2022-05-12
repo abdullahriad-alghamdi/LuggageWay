3d1d4cf8c13c642991a9f0e76528368bd75b99fa
abdulaziz <s391902003@ucj.edu.sa>
2022-05-12 20:45:37 +0300

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,6 @@ let videoBtn = document.querySelectorAll('.vid-btn');
 
 
 
-
 //If you scroll down on the menu or login page, the page will be
 // removed and you will be redirected to the home page.
 window.onscroll = () => {
